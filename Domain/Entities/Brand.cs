470cfa9cb52833e77d94fcb3cfb3a35a124c8d58
Resul Pala <resulpala@hotmail.com.tr>
2023-09-03 01:04:45 +0300

    Add project files.

---
+++ Domain/Entities/Brand.cs
@@ -0,0 +1,13 @@
+﻿using Core.Persistance.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity;
+
+public class Brand : Entity<Guid>
+{
+    public string Name { get; set; }
+}
