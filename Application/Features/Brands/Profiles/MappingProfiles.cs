551010ab701bb92b6240cb5407019d6b0cf20921
Resul Pala <resulpala@hotmail.com.tr>
2023-09-04 23:24:37 +0300

    Respond Request Pattern implemented, CRUD for brand domain added

--- Application/Features/Brands/Profiles/MappingProfiles.cs
+++ Application/Features/Brands/Profiles/MappingProfiles.cs
@@ -1,5 +1,11 @@
 ﻿using Application.Features.Brands.Commands.Create;
+using Application.Features.Brands.Commands.Delete;
+using Application.Features.Brands.Commands.Update;
+using Application.Features.Brands.Queries.GetById;
+using Application.Features.Brands.Queries.GetList;
 using AutoMapper;
+using Core.Application.Responses;
+using Core.Persistance.Paging;
 using Domain.Entity;
 using System;
 using System.Collections.Generic;
@@ -16,5 +22,16 @@ public class MappingProfiles : Profile
         CreateMap<Brand, CreateBrandCommand>().ReverseMap();
         CreateMap<Brand, CreatedBrandResponse>().ReverseMap();
 
+        CreateMap<Brand, UpdateBrandCommand>().ReverseMap();
+        CreateMap<Brand, UpdatedBrandResponse>().ReverseMap();
+
+        CreateMap<Brand, DeleteBrandCommand>().ReverseMap();
+        CreateMap<Brand, DeletedBrandResponse>().ReverseMap();
+
+        CreateMap<Brand, GetListBrandListItemDto>().ReverseMap();
+        CreateMap<Paginate<Brand>, GetListResponse<GetListBrandListItemDto>>().ReverseMap();
+
+        CreateMap<Brand, GetByIdBrandResponse>().ReverseMap();
+        
     }
 }
